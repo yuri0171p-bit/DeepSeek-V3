5043f69f7d1e18a08b8087c82f60ea6756775518
yuri0171p-bit <yuri0171p@gmail.com>
2026-06-07 17:48:40 -0300

    Update generate.py

--- inference/generate.py
+++ inference/generate.py
@@ -115,7 +115,7 @@ def main(
     with torch.device("cuda"):
         model = Transformer(args)
     tokenizer = AutoTokenizer.from_pretrained(ckpt_path)
-    tokenizer.decode(generate(model, [tokenizer.encode("DeepSeek")], 2, -1, 1.)[0])
+    tokenizer.decode(generate(model, [ia teste], 2, -1, 1.)[0])
     load_model(model, os.path.join(ckpt_path, f"model{rank}-mp{world_size}.safetensors"))
 
     if interactive:
